9fd2ac8f5541b2f37aee7ba40f91e2fe9f02ccc9
Mohd Sanabil <mohdsanabil4557@gmail.com>
2023-04-07 15:37:29 +0530

    Error solving

--- Student_data/Student_data/settings.py
+++ Student_data/Student_data/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'CsrfViewMiddleware'
 ]
 
 ROOT_URLCONF = 'Student_data.urls'
